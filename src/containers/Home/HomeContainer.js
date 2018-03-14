bd955467950d1f6d2aaf3c17d7a831708979a474
Nguyen Tuan <tuannguyen@teqnological.asia>
2018-03-14 13:02:14 +0700

    Add home page

---
+++ src/containers/Home/HomeContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Home from '../../components/Home/Home';
+
+class HomeContainer extends Component {
+  render() {
+    return (
+      <Home />
+    );
+  }
+}
+
+export default connect()(HomeContainer);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,15 @@
 import React, { Fragment } from 'react';
-import { Route } from 'react-router-dom';
-import DashboardContainer from '../containers/DashboardContainer';
+import { Route, BrowserRouter } from 'react-router-dom';
+import HomeContainer from '../containers/Home/HomeContainer';
 import LoginContainer from '../containers/LoginContainer';
 import AuthenticatedRoute from './AuthenticatedRoute';
 
 export default function configRoutes() {
   return (
     <Fragment>
-      <AuthenticatedRoute exact path="/" component={DashboardContainer} />
+      <BrowserRouter basename="/account">
+        <AuthenticatedRoute exact path="/" component={HomeContainer} />
+      </BrowserRouter>
       <Route exact path="/login" component={LoginContainer} />
     </Fragment>
   );
