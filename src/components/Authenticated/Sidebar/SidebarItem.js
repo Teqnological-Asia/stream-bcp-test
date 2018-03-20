0d48e73319c7136f43cd991019912b1b4c8e72ab
Nguyen Tuan <tuannguyen@teqnological.asia>
2018-03-20 12:44:56 +0700

    Use Link for menu

--- src/components/Authenticated/Sidebar/SidebarItem.js
+++ src/components/Authenticated/Sidebar/SidebarItem.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SidebarItem = ({name, href, activeHrefs, currentPathName}) => (
   <dd className={(activeHrefs.concat(href).includes(currentPathName.replace(/\/+$/, '')) == true) ? 'is_current' : ''}>
-    <a href={href}>{name}</a>
+    <Link to={href}>{name}</Link>
   </dd>
 );
 
