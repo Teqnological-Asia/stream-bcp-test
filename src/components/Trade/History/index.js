2083b83748bc4de6e9740dfb99e5cffadb492806
Nguyen Tuan <tuannguyen@teqnological.asia>
2018-03-27 13:18:37 +0700

    Update checkbox

--- src/components/Trade/History/index.js
+++ src/components/Trade/History/index.js
@@ -14,8 +14,16 @@ class TradeHistory extends Component {
 
     this.state = {
       from: fromDate,
-      to: toDate
-    }
+      to: toDate,
+      checkAll: false,
+      equity: false,
+      shipment_receipt: false,
+      divident: false,
+      deposit: false,
+      withdraw: false
+    };
+
+    this.types = ['equity', 'shipment_receipt', 'divident', 'deposit', 'withdraw'];
   }
 
   componentWillMount() {
@@ -27,12 +35,36 @@ class TradeHistory extends Component {
   }
 
   handleSearch = (e) => {
-    this.loadTradeHistories();
+    let typeValues = this.types.map(type => this.state[type]);
+    let isUncheckAllTypes = typeValues.every((value) => {return value === false});
+    if (isUncheckAllTypes && !this.state.checkAll) {
+      alert('error');
+    } else {
+      this.loadTradeHistories();
+    }
+  }
+
+  handleCheckType = (e) => {
+    const target = e.target;
+    const name = target.name;
+    const value = target.checked;
+    this.setState({[name]: value});
+    this.setState({checkAll: false});
+  }
+
+  handleCheckAllTypes = (e) => {
+    const target = e.target;
+    const value = target.checked;
+    this.setState({checkAll: value});
+
+    this.types.forEach((type) => {
+      this.setState({[type]: value});
+    });
   }
 
   loadTradeHistories = (page=1) => {
     let params = {page: page};
-    let { from, to } = this.state;
+    const { from, to, checkAll } = this.state;
 
     if (from) {
       params.from = moment(from).format('YYYYMMDD');
@@ -40,13 +72,22 @@ class TradeHistory extends Component {
     if (to) {
       params.to = moment(to).format('YYYYMMDD');
     }
+    if (!checkAll) {
+      let typeParams = [];
+      this.types.forEach((type) => {
+        if (this.state[type] === true) {
+          typeParams.push(...type.split('_'));
+        }
+      });
+      params.type = typeParams;
+    }
 
     this.props.loadTradeHistoriesRequest(params);
   }
 
   render() {
     const { tradeHistories, currentPage, totalPages } = this.props;
-    const { from, to } = this.state;
+    const { from, to, checkAll, equity, shipment_receipt, divident, deposit, withdraw } = this.state;
     const showPagination = tradeHistories.length > 0;
 
     const pagination = (
@@ -86,25 +127,25 @@ class TradeHistory extends Component {
               </div>
               <div className="p-section_search_item_body">
                 <label className="p-form-object_label">
-                  <input type="checkbox"/>すべて
+                  <input type="checkbox" checked={checkAll} onChange={this.handleCheckAllTypes}/>すべて
                 </label>
                 <label className="p-form-object_label">
-                  <input type="checkbox"/>現物
+                  <input type="checkbox" checked={equity} name="equity" onChange={this.handleCheckType}/>現物
                 </label>
-                <label className="p-form-object_label">
+                {/* <label className="p-form-object_label">
                   <input type="checkbox"/>譲渡益税
+                </label> */}
+                <label className="p-form-object_label">
+                  <input type="checkbox" checked={shipment_receipt} name="shipment_receipt" onChange={this.handleCheckType}/>入出庫
                 </label>
                 <label className="p-form-object_label">
-                  <input type="checkbox"/>入出庫
+                  <input type="checkbox" checked={divident} name="divident" onChange={this.handleCheckType}/>配当金
                 </label>
                 <label className="p-form-object_label">
-                  <input type="checkbox"/>配当金
+                  <input type="checkbox" checked={deposit} name="deposit" onChange={this.handleCheckType}/>入金
                 </label>
                 <label className="p-form-object_label">
-                  <input type="checkbox"/>入金
-                </label>
-                <label className="p-form-object_label">
-                  <input type="checkbox"/>出金
+                  <input type="checkbox" checked={withdraw} name="withdraw" onChange={this.handleCheckType}/>出金
                 </label>
               </div>
             </div>
