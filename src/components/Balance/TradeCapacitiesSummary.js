ca5dbac22ec4fef44d89740565603201f514598e
Le Sanh Phuc <phucls288@gmail.com>
2018-04-05 11:29:00 +0800

    Update current date time for Balance

--- src/components/Balance/TradeCapacitiesSummary.js
+++ src/components/Balance/TradeCapacitiesSummary.js
@@ -1,44 +1,57 @@
-import React from 'react';
-import { formatNumber } from '../../utils';
+import React, { Component }  from 'react';
+import { formatNumber, formatDateTime } from '../../utils';
 
-const TradeCapacitiesSummary = ({tradeCapacities}) => {
-  if (tradeCapacities === undefined || tradeCapacities.length === 0) {
-    return null;
+class TradeCapacitiesSummary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      curDateTime: new Date(),
+    };
   }
-  return (
-    <div className="p-lives u-mt40p">
-      <div className="p-section_header">
-        <div className="p-section_header_title">口座余力</div>
-        <div className="p-section_header_aside">2018/02/20 14:30（<a className="icon-arrows-ccw" href="">更新</a>）</div>
-      </div>
-      <div className="p-life">
-        <div className="p-life_head">買付余力</div>
-        <div className="p-life_body">
-          {formatNumber(tradeCapacities[0].equity_trading_power)}
-          <span className="p-unit">円</span>
+
+  render() {
+    if (this.props.tradeCapacities === undefined || this.props.tradeCapacities.length === 0) {
+      return null;
+    }
+    
+    return (
+      <div className="p-lives u-mt40p">
+        <div className="p-section_header">
+          <div className="p-section_header_title">口座余力</div>
+          <div className="p-section_header_aside">
+            <span> {formatDateTime(this.state.curDateTime)} </span>         
+            (<a className="icon-arrows-ccw" onClick={() => this.setState({curDateTime: new Date()})}>更新</a>）
+          </div>
         </div>
-      </div>
-      {/*
-      <div className="p-life">
-        <div className="p-life_head"> 信用余力</div>
-        <div className="p-life_body">3,000,000<span className="p-unit">円</span></div>
-      </div>
-      */}
-      <div className="p-life">
-        <div className="p-life_head"> 出金可能額</div>
-        <div className="p-life_body">
-          {formatNumber(tradeCapacities[0].withdrawable)}
-          <span className="p-unit">円</span>
+        <div className="p-life">
+          <div className="p-life_head">買付余力</div>
+          <div className="p-life_body">
+            {formatNumber(this.props.tradeCapacities[0].equity_trading_power)}
+            <span className="p-unit">円</span>
+          </div>
         </div>
+        {/*
+        <div className="p-life">
+          <div className="p-life_head"> 信用余力</div>
+          <div className="p-life_body">3,000,000<span className="p-unit">円</span></div>
+        </div>
+        */}
+        <div className="p-life">
+          <div className="p-life_head"> 出金可能額</div>
+          <div className="p-life_body">
+            {formatNumber(this.props.tradeCapacities[0].withdrawable)}
+            <span className="p-unit">円</span>
+          </div>
+        </div>
+        {/*
+        <div className="p-life">
+          <div className="p-life_head"> 現引可能額</div>
+          <div className="p-life_body">1,000,000<span className="p-unit">円</span></div>
+        </div>
+        */}
       </div>
-      {/*
-      <div className="p-life">
-        <div className="p-life_head"> 現引可能額</div>
-        <div className="p-life_body">1,000,000<span className="p-unit">円</span></div>
-      </div>
-      */}
-    </div>
-  );
+    );
+  }
 }
 
 export default TradeCapacitiesSummary;
