2aba4a33e850e6e873823c33e004f3301683c8c0
Nguyen Tuan <tuannguyen@teqnological.asia>
2018-04-27 14:43:03 +0700

    Update

--- src/components/Login/LoginFooter.js
+++ src/components/Login/LoginFooter.js
@@ -5,6 +5,7 @@ const LoginFooter = () => (
   <div className="p-container_panel_foot">
     <Link to="/account/reminder" className="p-container_panel_link">パスワードをお忘れの方はこちら</Link>
     <a className="p-container_panel_link" href="https://smartplus-sec.com" target="_blank" rel="noopener noreferrer">そのほかでお困りの方はこちら</a>
+    <a className="p-container_panel_link" href="https://help.smartplus-sec.com/s/article/systemreqs" target="_blank" rel="noopener noreferrer">動作環境に関してはこちら</a>
   </div>
 )
 
