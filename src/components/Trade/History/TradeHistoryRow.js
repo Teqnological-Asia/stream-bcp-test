2a36a57af6bfeb1cf680e9afa63b234a7563a41d
Nguyen Thuy <thuynguyen@teqnlogical.asia>
2018-09-13 04:37:58 +0700

    Update logic margin

--- src/components/Trade/History/TradeHistoryRow.js
+++ src/components/Trade/History/TradeHistoryRow.js
@@ -5,6 +5,9 @@ const TradeHistoryRow = ({tradeHistory}) => {
   const formatTradeType = (tradeType) => {
     const tradeTypes = {
       'equity': '現物取引',
+      'margin_open': '信用新規',
+      'margin_close': '信用返済',
+      'margin_swap': '現引/現渡',
       'withdraw': '出金',
       'deposit': '入金',
       'shipment': '出庫',
@@ -54,11 +57,14 @@ const TradeHistoryRow = ({tradeHistory}) => {
     return '';
   }
 
-  const formatQuantity = (value) => {
+  const formatQuantity = (value, trade_type) => {
+    const types = ["equity","margin_open","margin_close","margin_swap","shipment","receipt"];
     if (value) {
-      return parseInt(value);
+      if (types.includes(trade_type)) {
+        return parseInt(value);
+      }
     }
-    return value;
+    return "";
   }
 
   const tradeDetail = tradeHistory.trade_detail;
@@ -78,7 +84,7 @@ const TradeHistoryRow = ({tradeHistory}) => {
       <td className="c-l">{formatAccountType(tradeDetail.account_type)}</td>
       <td className="c-l">{formatSide(tradeDetail.side, tradeHistory.trade_type)}</td>
       <td className="c-l">{tradeDetail.stock_name}</td>
-      <td className={"c-r " + (tradeDetail.quantity < 0 ? 'u-minus' : '')}>{formatQuantity(tradeDetail.quantity)}</td>
+      <td className={"c-r " + (tradeDetail.quantity < 0 ? 'u-minus' : '')}>{formatQuantity(tradeDetail.quantity, tradeHistory.trade_type)}</td>
       <td className={"c-r " + (tradeDetail.unit_price < 0 ? 'u-minus' : '')}>{formatCurrency(tradeDetail.unit_price, 4)}</td>
       <td className={"c-r " + (tradeDetail.fee < 0 ? 'u-minus' : '')}>
         {
