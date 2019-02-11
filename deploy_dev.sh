6a6581359d4ef2013aeab0a761de0e96d7f70199
minhluong <minhluong@teqnological.asia>
2019-02-11 11:28:56 +0700

    Deploy dev with script

---
+++ deploy_dev.sh
@@ -0,0 +1,31 @@
+BUILD=true
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+for i in "$@"
+do
+case $i in
+    -b=*|--build=*)
+    BUILD="${i#*=}"
+    ;;
+    --default)
+    DEFAULT=YES
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+done
+
+# Build bcp source
+if $BUILD
+then
+echo -e "${GREEN}Building..${NC}"
+yarn build:development
+fi
+
+yarn --cwd ../web-open-account
+yarn --cwd ../web-open-account build:development
+cp -r ../web-open-account/build/ ./build/op/
+aws s3 sync build/ s3://web-bcp-dev/
+echo -e "${GREEN}Deploy done${NC}"
