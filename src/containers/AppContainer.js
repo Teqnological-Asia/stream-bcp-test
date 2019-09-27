701304485557e522882d4e4c6500c15ca57537b1
minhluong <minhluong@teqnological.asia>
2019-09-27 18:27:37 +0900

    Change account status to v3

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -6,25 +6,28 @@ import { getAuthHeader } from '../actions/auth';
 class AppContainer extends Component {
   componentDidMount() {
     if (sessionStorage.getItem('token') !== null && window.location.pathname.match('/account')) {
-      const url = `${process.env.REACT_APP_OPENACCOUNT_API_HOST}/account/status`
+      const url = `${process.env.REACT_APP_OPENACCOUNT_API_HOST}/v3/accounts/status`
       const options = {
         headers: getAuthHeader()
       }
-      axios.post(url, {}, options)
-      .then(({ data: { data: { items } } }) => {
-        const { account_status, identification_status, progress_status } = items
-        if (account_status === 1) {
+      axios.get(url, options).then(({ data: { data: { items } } }) => {
+        const {
+          account_status, identification_status, progress_status,
+          posted_status, identification_messages, profile_messages
+        } = items
+        if (account_status === 'available') {
           sessionStorage.setItem('account_status', account_status)
         } else {
           sessionStorage.setItem('account_status', account_status)
           sessionStorage.setItem('identification_status', identification_status)
           sessionStorage.setItem('progress_status', progress_status)
+          sessionStorage.setItem('posted_status', posted_status)
+          sessionStorage.setItem('identification_messages', JSON.stringify(identification_messages))
+          sessionStorage.setItem('profile_messages', JSON.stringify(profile_messages))
           sessionStorage.setItem('path', '/openaccount/check-status')
           window.location.href = '/op/index.html'
         }
-      })
-      .catch(error => {
-      })
+      }).catch(error => {})
     }
   }
 
